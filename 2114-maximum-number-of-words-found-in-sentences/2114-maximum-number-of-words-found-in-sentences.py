a7bd7f06d743e12b7d3c2d909219d038d95dfbab
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-07-24 16:24:27 +0700

    Update 2114-maximum-number-of-words-found-in-sentences.py

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.py
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.py
@@ -4,3 +4,18 @@ class Solution:
         
         max_words = max(len(sen.split()) for sen in sentences)
         return max_words
+
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        self.sentences = sentences
+        max_words = 0  # Initialize the max_words to 0
+
+        for sen in sentences:
+            words = sen.split(' ')
+            num_words = len(words)
+
+            if num_words > max_words:
+                max_words = num_words
+
+        return max_words
+
