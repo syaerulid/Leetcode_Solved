8ded877ba7fb5a5d9533956f9af3779aa5fc3e38
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-07-27 11:14:27 +0700

    Time: 64 ms (30.70%), Space: 16.3 MB (82.64%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        new_list = []
+        number_list = []
+        new_number_list = []
+        output = 0
+        for det in details:
+            detail = det[10:13]
+            new_list.append(detail)
+        for new in new_list:
+            num = new[1:]
+            number_list.append(num)
+            new_number_list = list(map(int, number_list))
+        for num in new_number_list:
+            if num > 60:
+                output += 1
+            else:
+                output += 0
+
+        return output
+                
+
