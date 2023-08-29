6a79b28a62a70bb9b222bc5048c5b6810550e1d1
Erul - Bukan Irul <119069839+syaerulid@users.noreply.github.com>
2023-08-30 04:59:15 +0700

    Time: 58 ms (79.93%), Space: 16.1 MB (99.22%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
@@ -2,8 +2,8 @@ class Solution:
     def findMatrix(self, nums: List[int]) -> List[List[int]]:
         ans = []
         while len(nums) > 0:
-            s = set(nums)
-            ans.append(list(s))
+            s = set(nums)  # Create a set of unique values from nums
+            ans.append(list(s))  # Append the list of unique values to ans
             for each in s:
-                nums.remove(each)
+                nums.remove(each)  # Remove each unique value from nums
         return ans
